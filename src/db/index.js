81f8f9b70a4d2ccc30bf5f1474e5dc3f8e03a3ca
manjul01 <manjulverma131@gmail.com>
2026-03-23 18:26:31 +0530

    connect to mongodb atlas

---
+++ src/db/index.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB=async()=>{
+    try{
+     const connectionInstance=   await mongoose.connect(`${process.env.MONGO_URL}/${DB_NAME}`);
+     console.log(`\n MongoDB connected !! DB host: ${connectionInstance.connection.host} \n`);
+    }
+    catch(err){
+        console.log("MONGODB connection failed !!");
+        process.exit(1);
+    }
+
+}
+export default connectDB;
